be80d91f3b9b482c2efdbdded029bd997a8c2efe
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-31 16:17:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let input = document.querySelector(".search");
+let btn = document.querySelector(".submit")
 
-
-fetch("https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}")
+fetch("https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=9a01012f8fa0847a6c1da9bd58353c95")
