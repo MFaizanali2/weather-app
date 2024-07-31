e79bbeb8394bbf38c36215495f59c1575bc899f9
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-31 16:45:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
-        // search input area
-let input = document.querySelector(".search");
-let btn = document.querySelector(".submit");
+// search input area
+let input = document.getElementById("search");
+let btn = document.getElementById("submit");
 
 
-        // container area
-let temperature = document.querySelector(".temp");
-let cityname = document.querySelector(".name");
-let countryname = document.querySelector(".country");
-let time = document.querySelector(".time");
-let date = document.querySelector(".date");
-let icon = document.querySelector(".icon");
-let condition = document.querySelector(".condition");
+
+    // container area
+let temperature = document.getElementById("temp");
+let cityname = document.getElementById("name");
+let countryname = document.getElementById("country");
+let time = document.getElementById("time");
+let date = document.getElementById("date");
+let icon = document.getElementById("icon");
+let condition = document.getElementById("condition");
 
 
-        // second area
-let cloud = document.querySelector(".clouds");
-let humidity = document.querySelector(".humids");
-let wind = document.querySelector(".winds");
+// second area
+let cloud = document.getElementById("clouds");
+let humidity = document.getElementById("humids");
+let wind = document.getElementById("winds");
 
 
-function fetchdata(){
-    console.log(input.value)
+function data() {
+    let url = ("https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=9a01012f8fa0847a6c1da9bd58353c95")
+    fetch(url)
+    .then((res) =>{
+        return res.json();
+    })
+    .then((data) =>{
+        console.log(data);
+    })
+    .catch((err) =>{
+        console.log(err)
+    })
+
+    
 }
-btn.addEventListener("click", fetchdata);
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=9a01012f8fa0847a6c1da9bd58353c95")
+btn.addEventListener("click", data);
